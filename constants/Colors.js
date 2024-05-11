3f03286aa870e647705306075b734dfacb306f2a
koleks92 <koleks92@gmail.com>
2024-05-11 09:02:59 +0200

    feat: colors

---
+++ constants/Colors.js
@@ -0,0 +1,12 @@
+// Global colors
+
+export const Colors = {
+    lightGreen: '#8BC34A',
+    mediumGreen: '#4CAF50',
+    darkGreen: '#388E3C',
+    paleGreen: '#C8E6C9',
+    oliveGreen: '#6B8E23',
+    neutralGrey: '#BDBDBD',
+    warmWhite: '#F5F5F5',
+    
+}
