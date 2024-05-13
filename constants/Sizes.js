25371033ae0e0fd384768eb9e9081a43894692d8
koleks92 <koleks92@gmail.com>
2024-05-13 19:54:04 +0200

    feat: scrW, scrH

--- constants/Sizes.js
+++ constants/Sizes.js
@@ -4,7 +4,9 @@ const scrH = Dimensions.get("screen").height;
 const scrW = Dimensions.get("screen").width;
 
 export const Sizes = {
-    topButtonSize: scrH * 0.04,     // Top Options Buttons Size
+    scrH: scrH,
+    scrW: scrW,
+    topButtonSize: scrH * 0.05,     // Top Options Buttons Size
     addTaskHeight: scrH * 0.5,      // Add Task Modal Height
     addTaskWidth: scrW * 0.8,       // Add Task Modal Width
 }
