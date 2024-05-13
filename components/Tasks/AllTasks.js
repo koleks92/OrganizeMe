b47b57113028b5ebde13e2758551903c4e055d6c
koleks92 <koleks92@gmail.com>
2024-05-13 19:24:58 +0200

    feat: all task component init

---
+++ components/Tasks/AllTasks.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function AllTasks() {
+    return (
+        <View>
+            <Text>All Tasks</Text>
+        </View>
+    )
+};
+
+export default AllTasks;
+
+const styles = StyleSheet.create({
+
+})
