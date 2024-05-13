f59095a47dd825fff5d076118a11cfddca8fba30
koleks92 <koleks92@gmail.com>
2024-05-13 19:01:54 +0200

    feat: add task modal sizes

--- constants/Sizes.js
+++ constants/Sizes.js
@@ -4,5 +4,7 @@ const scrH = Dimensions.get("screen").height;
 const scrW = Dimensions.get("screen").width;
 
 export const Sizes = {
-    topButtonSize: scrH * 0.04
+    topButtonSize: scrH * 0.04,     // Top Options Buttons Size
+    addTaskHeight: scrH * 0.5,      // Add Task Modal Height
+    addTaskWidth: scrW * 0.8,       // Add Task Modal Width
 }
