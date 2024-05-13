81c2ac26c6bc558fd1d2d70c1a2e52fadf4f2b69
koleks92 <koleks92@gmail.com>
2024-05-13 12:24:03 +0200

    fix: Top button size moved to Sizes.js

--- app/history.js
+++ app/history.js
@@ -2,14 +2,14 @@
 
 import { DrawerActions } from "@react-navigation/native";
 import { useNavigation } from "expo-router";
-import { Text, Dimensions} from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Background from "../components/UI/Background";
 import ButtonCustom from "../components/UI/ButtonCustom";
 import Top from "../components/UI/Top";
 import MenuButton from "../components/UI/MenuButton";
+import { Sizes } from "../constants/Sizes";
 
-const scrH = Dimensions.get("screen").height;
 
 function History() {
     const navigation = useNavigation();
@@ -17,15 +17,13 @@ function History() {
         navigation.dispatch(DrawerActions.openDrawer());
     };
 
-    // Buttons size
-    const buttonSize = scrH * 0.04;
 
     return (
         <Background>
             <SafeAreaView>
                 <Top>
                     <ButtonCustom onPress={openDrawer}>
-                        <MenuButton size={buttonSize} />
+                        <MenuButton size={Sizes.topButtonSize} />
                     </ButtonCustom>
                 </Top>
                 <Text>History</Text>
