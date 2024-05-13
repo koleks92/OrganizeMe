d63668948a7f51f5d53f76af425ba918992df8b3
koleks92 <koleks92@gmail.com>
2024-05-13 20:03:43 +0200

    feat: close button

--- components/AddTask/AddTask.js
+++ components/AddTask/AddTask.js
@@ -1,14 +1,54 @@
-// Add task for index modal 
+import { View, Text, Button, StyleSheet } from "react-native";
+import { Sizes } from "../../constants/Sizes";
+import { Colors } from "../../constants/Colors";
+import Top from "../UI/Top";
+import ButtonCustom from "../UI/ButtonCustom";
+import { Ionicons } from "@expo/vector-icons";
 
-import { View, Text, Button } from "react-native";
 
-function AddTask({closeModal}) {
+function AddTask({ closeModal }) {
     return (
-        <View>
-            <Text>Add New Task</Text>
-            <Button onPress={closeModal} title="Close Modal" />
+        <View style={styles.shadowWrapper}>
+            <View style={styles.root}>
+                <View style={styles.topView}>
+                    <View></View>
+                    <ButtonCustom onPress={closeModal}><Ionicons
+                            name="close-circle-outline"
+                            size={Sizes.topButtonSize}
+                            color={Colors.darkGreen}
+                        /></ButtonCustom>
+                </View>
+                <Text>Add New Task</Text>
+            </View>
         </View>
     );
 }
 
 export default AddTask;
+
+const styles = StyleSheet.create({
+    shadowWrapper: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 6,
+        elevation: 5,
+    },
+    root: {
+        borderWidth: 1,
+        borderColor: Colors.darkGreen,
+        borderRadius: 20,
+        width: Sizes.addTaskWidth,
+        height: Sizes.addTaskHeight,
+        backgroundColor: Colors.lightGreen,
+        padding: 0,
+    },
+    topView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 6
+    }
+});
