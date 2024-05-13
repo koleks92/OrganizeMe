07bcb5432e90b6bdd036b6133b9dd4dba0aecdda
koleks92 <koleks92@gmail.com>
2024-05-13 12:23:30 +0200

    feat: Sizes constants file

---
+++ constants/Sizes.js
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native";
+
+const scrH = Dimensions.get("screen").height;
+const scrW = Dimensions.get("screen").width;
+
+export const Sizes = {
+    topButtonSize: scrH * 0.04
+}
