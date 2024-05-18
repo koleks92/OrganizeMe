acdf70106d2b4f563f4be213d36926d1dcafb681
koleks92 <koleks92@gmail.com>
2024-05-18 08:39:48 +0200

    feat: topOptionsheight

--- constants/Sizes.js
+++ constants/Sizes.js
@@ -7,6 +7,7 @@ export const Sizes = {
     scrH: scrH,
     scrW: scrW,
     topButtonSize: scrH * 0.05,     // Top Options Buttons Size
+    topOptionsHeight: scrH * 0.05,  // Top Options Component Height
     addTaskHeight: scrH * 0.5,      // Add/Edit Task Modal Height
     addTaskWidth: scrW * 0.8,       // Add/Edit Task Modal Width
 }
