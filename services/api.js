153d8dda152b5cd6c050fc796b6fed13c65e2c58
koleks92 <koleks92@gmail.com>
2024-05-23 18:11:41 +0200

    feat: saveToDatabase

---
+++ services/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const url = "http://192.168.0.3:5000/newTask";
+
+export async function saveToDatabase(type, name, shop, extra, completed) {
+    try {
+        const response = await axios.post(url, {
+            type: type,
+            name: name,
+            shop: shop,
+            extra: extra,
+            completed: completed
+        })
+        console.log(response);
+    }
+    catch (error) {
+        console.error("Error: ", error)
+    }
+    
+    
+}
