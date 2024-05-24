c1532658f48dc2c4aabcf64a7025f7f5cc4ab496
koleks92 <koleks92@gmail.com>
2024-05-24 08:45:35 +0200

    feat: getAllTasks, getAllHistory, deleteTask, getTask

--- services/api.js
+++ services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const url = "http://192.168.0.3:5000/tasks";
+const urlHistory = "http://192.168.0.3:5000/history";
 
 export async function saveToDatabase(type, name, shop, extra, completed) {
     try {
@@ -18,4 +19,52 @@ export async function saveToDatabase(type, name, shop, extra, completed) {
     }
 }
 
-// Create delete function !
+// Get all uncompleted tasks
+
+export async function getAllTasks() {
+    try {
+        const response = await axios.get(url);
+        console.log(response)
+    } catch (error) {
+        console.error("Error: ", error)
+    }
+};
+
+// Get all history (completed tasks)
+
+export async function getAllHistory() {
+    try {
+        const response = await axios.get(urlHistory);
+        console.log(response)
+    } catch (error) {
+        console.error("Error: ", error)
+    }
+}
+
+// Delete one task by id
+
+export async function deleteTask(id) {
+    try {
+        const response = await axios.delete(url, id);
+        console.log(response);
+    } catch (error) {
+        console.error("Error:", error)
+    }
+}
+
+// Get one task by id
+
+export async function getTask(id) {
+    try {
+        const response = await axios.get(url + "/" + id)
+        console.log(response)
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+}
+
+// Edit task
+// TODO
+
+// Mark as completed/uncompleted
+// TODO
