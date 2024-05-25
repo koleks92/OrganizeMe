f66ea5a5b73901eff8c098a6c5c7287d35000c8f
koleks92 <koleks92@gmail.com>
2024-05-25 08:31:28 +0200

    feat: useState for different task type

--- components/Tasks/AllTasks.js
+++ components/Tasks/AllTasks.js
@@ -1,15 +1,59 @@
 import { StyleSheet, Text, View } from "react-native";
+import { getAllTasks } from "../../services/api";
+import { useEffect, useState } from "react";
 
 function AllTasks() {
+    const [doTasks, setDoTasks] = useState([]);
+    const [buyTasks, setBuyTasks] = useState([]);
+    const [sellTasks, setSellTasks] = useState([]);
+    const [checkTasks, setCheckTasks] = useState([]);
+
+    // Get all tasks
+    async function fetchTasks() {
+        try {
+            // Fetch tasks
+            const fetchedTasks = await getAllTasks();
+            // Save tasks
+
+            for (let i = 0; i < fetchedTasks.length; i++) {
+                let newTask = {
+                    id: fetchedTasks[i]._id,
+                    name: fetchedTasks[i].name,
+                    type: fetchedTasks[i].type,
+                    shop: fetchedTasks[i].shop,
+                    extra: fetchedTasks[i].extra,
+                    completed: fetchedTasks[i].completed,
+                };
+                
+                if (newTask.type == "do") {
+                    setDoTasks(prevTasks => [...prevTasks, newTask])
+                } else if (newTask.type == "buy") {
+                    setBuyTasks(prevTasks => [...prevTasks, newTask])
+                } else if (newTask.type == "sell") {
+                    setSellTasks(prevTasks => [...prevTasks, newTask])
+                } else if (newTask.type == "check") {
+                    setCheckTasks(prevTasks => [...prevTasks, newTask])
+                }
+            }
+        } catch (error) {
+            console.error("Error fetching tasks: ", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    console.log(sellTasks)
+
+
     return (
         <View>
             <Text>All Tasks</Text>
         </View>
-    )
-};
+    );
+}
 
 export default AllTasks;
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
