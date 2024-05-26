16041270c5238f978ce44742d4a3a735f716fd43
koleks92 <koleks92@gmail.com>
2024-05-26 09:47:04 +0200

    feat: tasksToRender component

--- components/Tasks/TasksGroup.js
+++ components/Tasks/TasksGroup.js
@@ -1,11 +1,19 @@
 import { View, Text } from "react-native";
+import Task from "./Task";
 
 function TaskGroup({type, tasks}) {
-    console.log(tasks)
+
+    const tasksToRender = tasks.map((task) => {
+        return (
+            <Task task={task} key={task.id} />
+        )
+    })
+    
 
     return (
         <View>
             <Text>{type}</Text>
+            {tasksToRender}
         </View>
     );
 }
