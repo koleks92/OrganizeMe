06715da447cd6dc9b4575c708062c8b69fd9a7e6
koleks92 <koleks92@gmail.com>
2024-05-27 17:47:07 +0200

    feat: taskHOrizontalMargin

--- constants/Sizes.js
+++ constants/Sizes.js
@@ -6,11 +6,12 @@ const scrW = Dimensions.get("screen").width;
 export const Sizes = {
     scrH: scrH,
     scrW: scrW,
-    topButtonSize: scrH * 0.05,     // Top Options Buttons Size
-    topOptionsHeight: scrH * 0.05,  // Top Options Component Height
-    addTaskHeight: scrH * 0.5,      // Add/Edit Task Modal Height
-    addTaskWidth: scrW * 0.8,       // Add/Edit Task Modal Width
-    marginMainView: scrW * 0.06,    // Margin for root view for screens,
-    taskSmallHeight: scrH * 0.02,   // Task height for TasksGroup
-    tasksViewMP: scrH * 0.01,       // Tasks View Margin Bottom/Padding
+    topButtonSize: scrH * 0.05,         // Top Options Buttons Size
+    topOptionsHeight: scrH * 0.05,      // Top Options Component Height
+    addTaskHeight: scrH * 0.5,          // Add/Edit Task Modal Height
+    addTaskWidth: scrW * 0.8,           // Add/Edit Task Modal Width
+    marginMainView: scrW * 0.06,        // Margin for root view for screens,
+    taskSmallHeight: scrH * 0.04,       // Task height for TasksGroup
+    tasksViewMP: scrH * 0.01,           // Tasks View Margin Bottom/Padding
+    taskHorizontalMargin: scrH * 0.002  // Horizontal margin for each task view in tasksGroup
 }
