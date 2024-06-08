1d53b7ef4f3fc13b51f714fb497481ca7901c098
koleks92 <koleks92@gmail.com>
2024-06-08 08:41:33 +0200

    feat: context provider

--- app/_layout.js
+++ app/_layout.js
@@ -3,6 +3,7 @@ import { Drawer } from "expo-router/drawer";
 import { Ionicons } from "@expo/vector-icons";
 import { Dimensions } from "react-native";
 import { Colors } from "../constants/Colors";
+import OrganizeMeContext from "../store/Context";
 
 // Screen size
 const scrH = Dimensions.get("window").height;
@@ -13,66 +14,67 @@ const labelSize = scrH * 0.025;
 
 export default function Layout() {
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <Drawer
-                screenOptions={{
-                    drawerStyle: {
-                        backgroundColor: Colors.mediumGreen,
-                        width: scrW * 0.6,
-                        borderRightWidth: 0.5,
-                        borderRightColor: Colors.darkGreen,    
-                    },
-                    drawerLabelStyle: {
-                        fontSize: labelSize,
-                    },
-                    drawerActiveTintColor: Colors.warmWhite,
-                    drawerActiveBackgroundColor: Colors.darkGreen,
-                }}
-            >
-                <Drawer.Screen
-                    name="index"
-                    options={{
-                        drawerLabel: "Tasks",
-                        headerShown: false,
-                        drawerIcon: ({color}) => (
-                            <Ionicons
-                                name="documents-outline"
-                                size={labelSize * 1.2}
-                                color={color}
-                            />
-                        ),
-                        
+        <OrganizeMeContext.Provider>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <Drawer
+                    screenOptions={{
+                        drawerStyle: {
+                            backgroundColor: Colors.mediumGreen,
+                            width: scrW * 0.6,
+                            borderRightWidth: 0.5,
+                            borderRightColor: Colors.darkGreen,
+                        },
+                        drawerLabelStyle: {
+                            fontSize: labelSize,
+                        },
+                        drawerActiveTintColor: Colors.warmWhite,
+                        drawerActiveBackgroundColor: Colors.darkGreen,
                     }}
-                />
-                <Drawer.Screen
-                    name="history"
-                    options={{
-                        drawerLabel: "History",
-                        headerShown: false,
-                        drawerIcon: ({ color }) => (
-                            <Ionicons
-                                name="time-outline"
-                                size={labelSize * 1.2}
-                                color={color}
-                            />
-                        ),
-                    }}
-                />
-                <Drawer.Screen
-                    name="about"
-                    options={{
-                        drawerLabel: "About",
-                        headerShown: false,
-                        drawerIcon: ({ color }) => (
-                            <Ionicons
-                                name="information-circle-outline"
-                                size={labelSize * 1.2}
-                                color={color}
-                            />
-                        ),
-                    }}
-                />
-            </Drawer>
-        </GestureHandlerRootView>
+                >
+                    <Drawer.Screen
+                        name="index"
+                        options={{
+                            drawerLabel: "Tasks",
+                            headerShown: false,
+                            drawerIcon: ({ color }) => (
+                                <Ionicons
+                                    name="documents-outline"
+                                    size={labelSize * 1.2}
+                                    color={color}
+                                />
+                            ),
+                        }}
+                    />
+                    <Drawer.Screen
+                        name="history"
+                        options={{
+                            drawerLabel: "History",
+                            headerShown: false,
+                            drawerIcon: ({ color }) => (
+                                <Ionicons
+                                    name="time-outline"
+                                    size={labelSize * 1.2}
+                                    color={color}
+                                />
+                            ),
+                        }}
+                    />
+                    <Drawer.Screen
+                        name="about"
+                        options={{
+                            drawerLabel: "About",
+                            headerShown: false,
+                            drawerIcon: ({ color }) => (
+                                <Ionicons
+                                    name="information-circle-outline"
+                                    size={labelSize * 1.2}
+                                    color={color}
+                                />
+                            ),
+                        }}
+                    />
+                </Drawer>
+            </GestureHandlerRootView>
+        </OrganizeMeContext.Provider>
     );
 }
