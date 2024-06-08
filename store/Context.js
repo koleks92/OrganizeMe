42a93623d8afd31b2b55aedaf440ed3f44456c2d
koleks92 <koleks92@gmail.com>
2024-06-08 08:46:05 +0200

    feat: taskContext, setTaskContext

--- store/Context.js
+++ store/Context.js
@@ -1,5 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { setShouldAnimateExitingForTag } from "react-native-reanimated/lib/typescript/reanimated2/core";
 
 const OrganizeMeContext = createContext();
+
+const OrganizeMeProvider = ({ children }) => {
+    const [task, setTask] = useState();
+
+    return (
+        <OrganizeMeContext.Provider value={{ taskContext, setTaskContext }}>
+            {children}
+        </OrganizeMeContext.Provider>
+    )
+}
 
 export default OrganizeMeContext;
