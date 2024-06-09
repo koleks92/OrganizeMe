ab6751276c2f162206cf849ad74df7bfaf093cf5
koleks92 <koleks92@gmail.com>
2024-06-09 16:03:50 +0200

    feat: new task handler

--- store/Context.js
+++ store/Context.js
@@ -3,11 +3,21 @@ import { createContext, useState } from "react";
 export const OrganizeMeContext = createContext();
 
 const OrganizeMeProvider = ({ children }) => {
-    const [newTaskData, setNewTaskData] = useState(null);
     const [newTask, setNewTask] = useState(false);
+    const [newTaskData, setNewTaskData] = useState(null)
+
+    const newTaskHandler = (task) => {
+        if (task) {
+            setNewTaskData(task);
+            setNewTask(true);
+        } else {
+            setNewTaskData(null);
+            setNewTask(false);
+        }
+    } 
 
     return (
-        <OrganizeMeContext.Provider value={{ newTaskData, setNewTaskData, newTask, setNewTask }}>
+        <OrganizeMeContext.Provider value={{ newTaskData, newTaskHandler, newTask, setNewTask, setNewTaskData }}>
             {children}
         </OrganizeMeContext.Provider>
     )
