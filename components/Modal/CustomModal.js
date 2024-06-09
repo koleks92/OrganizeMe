ce33662a6b6e7bc896758e8f9d15a80467eb5138
koleks92 <koleks92@gmail.com>
2024-06-09 16:46:13 +0200

    feat: custom modal component

---
+++ components/Modal/CustomModal.js
@@ -0,0 +1,37 @@
+import { View, StyleSheet } from "react-native";
+import { Colors } from "../../constants/Colors";
+import { Sizes } from "../../constants/Sizes";
+
+function CustomModal({children}) {
+    return (
+        <View style={styles.shadowWrapper}>
+            <View style={styles.root}>
+                {children}
+            </View>
+        </View>
+    )
+}
+
+export default CustomModal;
+
+const styles = StyleSheet.create({
+    shadowWrapper: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 6,
+    },
+    root: {
+        borderWidth: 1,
+        borderColor: Colors.darkGreen,
+        borderRadius: Sizes.scrH * 0.015,
+        width: Sizes.addTaskWidth,
+        height: Sizes.addTaskHeight,
+        backgroundColor: Colors.lightGreen,
+        padding: Sizes.scrH * 0.01,
+        elevation: 5
+    }
+});
