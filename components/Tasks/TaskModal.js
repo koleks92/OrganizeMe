0e83183c5a50aaee7464b3b3def6dd5918a9b39a
koleks92 <koleks92@gmail.com>
2024-06-09 16:40:10 +0200

    feat: basic modal styling

--- components/Tasks/TaskModal.js
+++ components/Tasks/TaskModal.js
@@ -1,11 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Colors } from "../../constants/Colors";
+import { Sizes } from "../../constants/Sizes";
 
-function TaskModal({task}) {
+function TaskModal({ task }) {
     return (
-        <View>
-            <Text>Yes</Text>
+        <View style={styles.shadowWrapper}>
+            <View style={styles.root}>
+                <Text>Yes</Text>
+            </View>
         </View>
-    )
-};
+    );
+}
 
 export default TaskModal;
+
+const styles = StyleSheet.create({
+    shadowWrapper: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 6,
+    },
+    root: {
+        borderWidth: 1,
+        borderColor: Colors.darkGreen,
+        borderRadius: Sizes.scrH * 0.015,
+        width: Sizes.addTaskWidth,
+        height: Sizes.addTaskHeight,
+        backgroundColor: Colors.lightGreen,
+        padding: Sizes.scrH * 0.01,
+        elevation: 5
+    },
+});
