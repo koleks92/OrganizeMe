06b4dfdff14df02397f8ce692fee5c330e24d91e
koleks92 <koleks92@gmail.com>
2024-06-09 08:52:02 +0200

    feat: newTaskData and newTask state

--- store/Context.js
+++ store/Context.js
@@ -3,10 +3,11 @@ import { createContext, useState } from "react";
 export const OrganizeMeContext = createContext();
 
 const OrganizeMeProvider = ({ children }) => {
-    const [taskContext, setTaskContext] = useState();
+    const [newTaskData, setNewTaskData] = useState();
+    const [newTask, setNewTask] = useState(false);
 
     return (
-        <OrganizeMeContext.Provider value={{ taskContext, setTaskContext }}>
+        <OrganizeMeContext.Provider value={{ newTaskData, setNewTaskData, newTask, setNewTask }}>
             {children}
         </OrganizeMeContext.Provider>
     )
