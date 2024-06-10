e93b40bf372713e55903415b41925d1be56bbb77
koleks92 <koleks92@gmail.com>
2024-06-10 22:35:41 +0200

    feat: topViewBar, delete task, HeadText

--- components/Tasks/TaskModal.js
+++ components/Tasks/TaskModal.js
@@ -1,14 +1,38 @@
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { Colors } from "../../constants/Colors";
 import { Sizes } from "../../constants/Sizes";
 import CustomModal from "../Modal/CustomModal";
 import ModalTopViewBar from "../Modal/ModalTopViewBar";
 import CloseButton from "../UI/CloseButton";
+import HeadText from "../Modal/HeadText";
+import ButtonCustom from "../UI/ButtonCustom";
+import { Ionicons } from "@expo/vector-icons";
+import { deleteTask } from "../../services/api";
 
-function TaskModal({ task, closeModal }) {
+
+function TaskModal({ task, closeModal, slideOutAnimation }) {
+
+    const deleteHandler = async () => {
+         try {
+            const response = await deleteTask(task.id);
+            closeModal();
+            slideOutAnimation()
+         } catch (error) {
+            console.error("Error: ", error);
+         }
+    }
+
     return (
         <CustomModal>
             <ModalTopViewBar>
+                <ButtonCustom onPress={deleteHandler}>
+                <Ionicons
+                        name="trash-outline"
+                        size={Sizes.topButtonSize * 0.9}
+                        color={Colors.darkGreen}
+                    />
+                </ButtonCustom>
+                <HeadText>{task.name}</HeadText>
                 <CloseButton onPress={closeModal} />
             </ModalTopViewBar>
         </CustomModal>
@@ -18,23 +42,5 @@ function TaskModal({ task, closeModal }) {
 export default TaskModal;
 
 const styles = StyleSheet.create({
-    shadowWrapper: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.5,
-        shadowRadius: 6,
-    },
-    root: {
-        borderWidth: 1,
-        borderColor: Colors.darkGreen,
-        borderRadius: Sizes.scrH * 0.015,
-        width: Sizes.addTaskWidth,
-        height: Sizes.addTaskHeight,
-        backgroundColor: Colors.lightGreen,
-        padding: Sizes.scrH * 0.01,
-        elevation: 5,
-    },
+
 });
