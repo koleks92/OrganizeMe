7507ae8b3165ce3a8b901a76e41e80a29dcedf98
koleks92 <koleks92@gmail.com>
2024-06-28 19:13:54 +0200

    feat: history view

--- app/history.js
+++ app/history.js
@@ -1,17 +1,27 @@
 // History page with tasks that was marked as done
 
-import { DrawerActions } from "@react-navigation/native";
+import { DrawerActions, useIsFocused } from "@react-navigation/native";
 import { useNavigation } from "expo-router";
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Background from "../components/UI/Background";
 import ButtonCustom from "../components/UI/ButtonCustom";
 import Top from "../components/UI/Top";
 import MenuButton from "../components/UI/MenuButton";
 import { Sizes } from "../constants/Sizes";
-
+import AllTasks from "../components/Tasks/AllTasks";
+import { useState, useEffect } from "react";
 
 function History() {
+        // Focused view
+        const isFocused = useIsFocused();
+
+        const [focused, setFocused] = useState(false);
+    
+        useEffect(() => {
+            setFocused(isFocused);
+        }, [isFocused]);
+
+    // Open side menu
     const navigation = useNavigation();
     const openDrawer = () => {
         navigation.dispatch(DrawerActions.openDrawer());
@@ -26,7 +36,7 @@ function History() {
                         <MenuButton size={Sizes.topButtonSize} />
                     </ButtonCustom>
                 </Top>
-                <Text>History</Text>
+                <AllTasks focused={focused} history={true} />
             </SafeAreaView>
         </Background>
     );
