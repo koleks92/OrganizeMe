6623e2b861b8f4e4cb1ea268fb48b6b647f5af40
koleks92 <koleks92@gmail.com>
2024-07-01 14:53:33 +0200

    feat: about page

--- app/about.js
+++ app/about.js
@@ -2,14 +2,16 @@
 
 import { DrawerActions } from "@react-navigation/native";
 import { useNavigation } from "expo-router";
-import { Text } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Background from "../components/UI/Background";
 import ButtonCustom from "../components/UI/ButtonCustom";
 import Top from "../components/UI/Top";
 import MenuButton from "../components/UI/MenuButton";
 import { Sizes } from "../constants/Sizes";
-
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../constants/Colors";
+import * as Linking from "expo-linking"; // Importing Linking from expo-linking
 
 function About() {
     const navigation = useNavigation();
@@ -17,18 +19,96 @@ function About() {
         navigation.dispatch(DrawerActions.openDrawer());
     };
 
+    const openLink = (link) => {
+        Linking.openURL(link).catch((err) =>
+            console.error("Couldn't load page", err)
+        );
+    };
+
+    const openMail = (mail) => {
+        Linking.openURL(`mailto:${mail}`).catch((err) =>
+            console.error("Failed to open email client", err)
+        );
+    };
+
     return (
         <Background>
-            <SafeAreaView>
+            <SafeAreaView style={styles.safeArea}>
                 <Top>
                     <ButtonCustom onPress={openDrawer}>
                         <MenuButton size={Sizes.topButtonSize} />
                     </ButtonCustom>
                 </Top>
-                <Text>About</Text>
+                <View style={styles.container}>
+                    <Text style={styles.textStyleSmall}>created by:</Text>
+                    <Text style={styles.textStyleBig}>
+                        Jan Sebastian Konieczek
+                    </Text>
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.textStyleSmall}>contact:</Text>
+                    <View style={styles.linksContainer}>
+                        <ButtonCustom
+                            onPress={() => openMail("konieczekjan@gmail.com")}
+                        >
+                            <Ionicons
+                                name="mail-outline"
+                                size={Sizes.topButtonSize}
+                                color={Colors.darkGreen}
+                            />
+                        </ButtonCustom>
+                        <ButtonCustom
+                            onPress={() =>
+                                openLink("https://github.com/koleks92")
+                            }
+                        >
+                            <Ionicons
+                                name="logo-github"
+                                size={Sizes.topButtonSize}
+                                color={Colors.darkGreen}
+                            />
+                        </ButtonCustom>
+                        <ButtonCustom
+                            onPress={() =>
+                                openLink(
+                                    "https://www.linkedin.com/in/jan-konieczek"
+                                )
+                            }
+                        >
+                            <Ionicons
+                                name="logo-linkedin"
+                                size={Sizes.topButtonSize}
+                                color={Colors.darkGreen}
+                            />
+                        </ButtonCustom>
+                    </View>
+                </View>
             </SafeAreaView>
         </Background>
     );
 }
 
 export default About;
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+    container: {
+        margin: Sizes.marginMainView,
+    },
+    textStyleBig: {
+        fontFamily: "RobotoMono",
+        fontSize: Sizes.scrH * 0.03,
+        marginHorizontal: Sizes.marginMainView,
+    },
+    textStyleSmall: {
+        fontFamily: "RobotoMono",
+        fontSize: Sizes.scrH * 0.025,
+    },
+    linksContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginHorizontal: Sizes.marginMainView,
+    },
+});
